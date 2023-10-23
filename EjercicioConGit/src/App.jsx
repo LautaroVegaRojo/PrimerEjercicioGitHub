a6228a876c11f5a7cb9dcde4c44767450258614a
Lautaro <marcosyvega@gmail.com>
2023-10-23 16:39:31 -0300

    router

--- EjercicioConGit/src/App.jsx
+++ EjercicioConGit/src/App.jsx
@@ -1,14 +1,24 @@
-import "./App.css";
-import Router from "./components/Router";
+import { BrowserRouter, Route, Router, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import HomeScreen from "./components/Pages/HomeScreen";
+import AboutScreen from "./components/Pages/AboutScreen";
+import NotFound from "./components/Pages/NotFound";
 
 function App() {
-
-
   return (
-    <>
-      <Router/>
-    </>
+    <div>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/about" element={<AboutScreen />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
 export default App;
+
+// hacer un router con un navbar con bootstrap con el cdn crear un componente navbar y un componente de rutas en donde viculas las rutas
